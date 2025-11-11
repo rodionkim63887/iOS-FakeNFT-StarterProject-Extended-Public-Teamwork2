22915c406ad7ab1e4f7bc40bd768198aa0382e55
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-11-11 18:23:14 +0300

    [Feature] Create static mock ProductInCart cell

--- iOS-FakeNFT-Extended/UI/CartView/Components/ProductInCart.swift
+++ iOS-FakeNFT-Extended/UI/CartView/Components/ProductInCart.swift
@@ -1,15 +1,44 @@
-//
-//  ProductInCart.swift
-//  iOS-FakeNFT-Extended
-//
-//  Created by Алексей Непряхин on 11.11.2025.
-//
-
 import SwiftUI
 
 struct ProductInCart: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            Image("MockNFTCard1")
+                .resizable()
+                .scaledToFit()
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Spring")
+                    .font(.system(size: 17, weight: .bold))
+                
+                HStack(spacing: 2) {
+                    ForEach(0..<5) { _ in
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 12, height: 12)
+                            .foregroundStyle(.yellow)
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Цена")
+                    .font(.system(size: 13))
+                
+                Text("1,78 ETH")
+                    .font(.system(size: 17, weight: .bold))
+            }
+            .padding(.vertical, 8)
+            
+            Spacer()
+            
+            Image("DeleteFromCart")
+                .frame(width: 40, height: 40)
+        }
+        .frame(height: 108)
     }
 }
 
