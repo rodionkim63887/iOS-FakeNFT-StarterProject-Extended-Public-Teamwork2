c1d3b76b096c21ffaa3868eb3a4af409629708ea
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-11-11 17:49:08 +0300

    [Feature] Add CartView files

---
+++ iOS-FakeNFT-Extended/UI/CartView/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Алексей Непряхин on 11.11.2025.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
---
+++ iOS-FakeNFT-Extended/UI/CartView/Components/ProductInCart.swift
@@ -0,0 +1,18 @@
+//
+//  ProductInCart.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Алексей Непряхин on 11.11.2025.
+//
+
+import SwiftUI
+
+struct ProductInCart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductInCart()
+}
