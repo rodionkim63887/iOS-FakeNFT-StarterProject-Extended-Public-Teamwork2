601bad6422daf356db47cff477d0085d645b2fd3
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-11-14 17:21:24 +0300

    [Fix] Return back part of UIColor extension

--- iOS-FakeNFT-Extended/DesignSystem/Colors.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Colors.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 extension Color {
     static let primaryColor = Color("primaryColor")
@@ -13,4 +14,52 @@ extension Color {
     static let blackUniversalColor = Color("blackUniversalColor")
     static let whiteUniversalColor = Color("whiteUniversalColor")
     static let yellowUniversalColor = Color("yellowUniversalColor")
+}
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let alpha, red, green, blue: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (alpha, red, green, blue) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (alpha, red, green, blue) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (alpha, red, green, blue) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (alpha, red, green, blue) = (255, 0, 0, 0)
+        }
+        self.init(
+            red: CGFloat(red) / 255,
+            green: CGFloat(green) / 255,
+            blue: CGFloat(blue) / 255,
+            alpha: CGFloat(alpha) / 255
+        )
+    }
+    
+    private static let yaBlackLight = UIColor(hexString: "1A1B22")
+    private static let yaBlackDark = UIColor.white
+    private static let yaLightGrayLight = UIColor(hexString: "#F7F7F8")
+    private static let yaLightGrayDark = UIColor(hexString: "#2C2C2E")
+    
+    static let segmentActive = UIColor { traits in
+        return traits.userInterfaceStyle == .dark
+        ? .yaBlackDark
+        : .yaBlackLight
+    }
+    
+    static let segmentInactive = UIColor { traits in
+        return traits.userInterfaceStyle == .dark
+        ? .yaLightGrayDark
+        : .yaLightGrayLight
+    }
+    
+    static let closeButton = UIColor { traits in
+        return traits.userInterfaceStyle == .dark
+        ? .yaBlackDark
+        : .yaBlackLight
+    }
 }
