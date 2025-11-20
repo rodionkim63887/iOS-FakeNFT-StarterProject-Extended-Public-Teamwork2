c2f6973fdacd6e652e619ead027b4ab86ab8d2bc
Rodion Kim <skyrood2@yandex.ru>
2025-11-20 16:24:04 +0900

    created models for nft and nftCollection

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-struct Nft: Decodable {
+struct Nft: Decodable, Identifiable {
     let id: String
+    let name: String
+    let description: String
+    let author: String
+    let price: Double
     let images: [URL]
+    let rating: Int
+    let createdAt: Date
 }
