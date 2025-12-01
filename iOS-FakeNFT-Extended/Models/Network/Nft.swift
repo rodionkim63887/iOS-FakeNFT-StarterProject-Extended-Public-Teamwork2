47d9cbce64fd98b52003055fa6f3de544be9a3ac
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-12-01 14:20:17 +0300

    [Feature] Add Cart Store and remove Cart View Model.
    # Conflicts:
    #	iOS-FakeNFT-Extended.xcodeproj/project.pbxproj
    #	iOS-FakeNFT-Extended/Models/Network/Nft.swift
    #	iOS-FakeNFT-Extended/UI/CartView/CartView.swift
    #	iOS-FakeNFT-Extended/UI/CartView/CartViewModel.swift
    #	iOS-FakeNFT-Extended/UI/CartView/Components/ProductInCart.swift
    #	iOS-FakeNFT-Extended/UI/CartView/NftDeletingView.swift
    #	iOS-FakeNFT-Extended/UI/TabBarView.swift

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -2,5 +2,11 @@ import Foundation
 
 struct Nft: Decodable {
     let id: String
-    let images: [URL]
+    let name: String
+    let description: String
+    let author: String
+    let price: Float
+    let images: [String]
+    let rating: Int
+    let createdAt: String
 }
