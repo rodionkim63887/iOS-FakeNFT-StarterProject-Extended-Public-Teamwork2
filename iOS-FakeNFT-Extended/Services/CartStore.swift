115ec0b4fce498924cb4181b7f933d901a90c51c
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-12-04 01:53:12 +0300

    [Feature] Add new default NFT value to cart

--- iOS-FakeNFT-Extended/Services/CartStore.swift
+++ iOS-FakeNFT-Extended/Services/CartStore.swift
@@ -13,7 +13,21 @@ final class CartStore {
                                 "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Finn/3.png"
                             ],
                             rating: 3,
-                            createdAt: "2023-10-08T07:43:22.944Z[GMT]")]
+                            createdAt: "2023-10-08T07:43:22.944Z[GMT]"),
+                        
+                        Nft(id: "cc74e9ab-2189-465f-a1a6-8405e07e9fe4",
+                            name: "James Burt",
+                            description: "eos habeo percipit duis malesuada",
+                            author: "https://exciting_pare.fakenfts.org/",
+                            price: 11.14,
+                            imagesUrlsString: [
+                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/1.png",
+                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/2.png",
+                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/3.png"
+                            ],
+                            rating: 2,
+                            createdAt: "2023-07-11T05:27:40.359Z[GMT]")]
+    
     var deletingAttempt = false
     var blur: CGFloat = 0
     var nftToDelete: Nft?
