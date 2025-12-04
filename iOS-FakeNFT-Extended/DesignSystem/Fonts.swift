8487f57e91eeb28bf4c2b531356d39e35e2bd011
Rodion Kim <skyrood2@yandex.ru>
2025-12-04 21:41:18 +0900

    Added missing fonts to Fonts extension

--- iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Fonts.swift
@@ -4,8 +4,11 @@ extension Font {
     static let regular13: Font = .system(size: 13, weight: .regular)
     static let regular15: Font = .system(size: 15, weight: .regular)
     static let regular17: Font = .system(size: 17, weight: .regular)
+    static let regular20: Font = .system(size: 20, weight: .regular)
     
     static let medium10: Font = .system(size: 10, weight: .medium)
+    
+    static let semibold20: Font = .system(size: 20, weight: .semibold)
     
     static let bold17: Font = .system(size: 17, weight: .bold)
     static let bold22: Font = .system(size: 22, weight: .bold)
