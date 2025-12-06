c4bafb527fd80a17d9d7f9a609a44ac95cb0390f
Rodion Kim <skyrood2@yandex.ru>
2025-12-06 13:39:36 +0900

    Moved Mock nft data to external file for reusability purposes

--- iOS-FakeNFT-Extended/Services/CartStore.swift
+++ iOS-FakeNFT-Extended/Services/CartStore.swift
@@ -2,42 +2,15 @@ import SwiftUI
 
 @Observable
 final class CartStore {
-    var items: [Nft] = [Nft(id: "7773e33c-ec15-4230-a102-92426a3a6d5a",
-                            name: "Rosario Dejesus",
-                            description: "explicari lobortis rutrum evertitur fugit convenire ligula",
-                            author: "https://unruffled_cohen.fakenfts.org/",
-                            price: 28.27,
-                            imagesUrlsString: [
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Finn/1.png",
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Finn/2.png",
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Finn/3.png"
-                            ],
-                            rating: 3,
-                            createdAt: "2023-10-08T07:43:22.944Z[GMT]"),
-                        
-                        Nft(id: "cc74e9ab-2189-465f-a1a6-8405e07e9fe4",
-                            name: "James Burt",
-                            description: "eos habeo percipit duis malesuada",
-                            author: "https://exciting_pare.fakenfts.org/",
-                            price: 11.14,
-                            imagesUrlsString: [
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/1.png",
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/2.png",
-                                "https://code.s3.yandex.net/Mobile/iOS/NFT/Blue/Clover/3.png"
-                            ],
-                            rating: 2,
-                            createdAt: "2023-07-11T05:27:40.359Z[GMT]")]
+    var items: [Nft] = []
     
     var deletingAttempt = false
     var blur: CGFloat = 0
     var nftToDelete: Nft?
     
     var nftToDeleteImage: URL? {
-        guard let urlString = nftToDelete?.imagesUrlsString[0],
-              let url = URL(string: urlString) else {
-            return nil
-        }
-        
+        guard let urlString = nftToDelete?.imagesUrlsString.first,
+              let url = URL(string: urlString) else { return nil }
         return url
     }
     
@@ -70,7 +43,7 @@ final class CartStore {
     }
 
     func add(_ nft: Nft) {
-        if items.first(where: { $0.id == nft.id }) == nil {
+        if !items.contains(where: { $0.id == nft.id }) {
             items.append(nft)
         }
     }
