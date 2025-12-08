8793fdd6773e5bc827accb00fa10ae27b43913ef
Rodion Kim <skyrood2@yandex.ru>
2025-12-08 23:52:35 +0900

    modified NetworkClient to work with x-www-form-urlencoded, modified UpdateUserLikesRequest to send data in x-www-form-urlencoded

--- iOS-FakeNFT-Extended/Services/Requests/UpdateUserLikesRequest.swift
+++ iOS-FakeNFT-Extended/Services/Requests/UpdateUserLikesRequest.swift
@@ -2,17 +2,24 @@ import Foundation
 
 struct UpdateUserLikesRequest: NetworkRequest {
     
-    let dto: Encodable?
+    let likedIds: [String]
 
-    init(likedIds: [String]) {
-        self.dto = StringArrayDTO(strings: likedIds)
-    }
-    
-    var httpMethod: HttpMethod {
-        .put
-    }
-    
+    var httpMethod: HttpMethod { .put }
+
     var endpoint: URL? {
         URL(string: "\(RequestConstants.baseURL)/api/v1/profile/1")
+    }
+
+    var headers: [String : String]? {
+        ["Content-Type": "application/x-www-form-urlencoded"]
+    }
+
+    var body: Data? {
+        if likedIds.isEmpty {
+            return "likes=".data(using: .utf8)
+        } else {
+            let str = likedIds.map { "likes=\($0)" }.joined(separator: "&")
+            return str.data(using: .utf8)
+        }
     }
 }
