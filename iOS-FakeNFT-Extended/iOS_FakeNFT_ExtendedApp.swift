ffbb9212f2fb22c869c745793b5caa9598789aae
Rodion Kim <skyrood2@yandex.ru>
2025-12-08 23:50:40 +0900

    Added UserLikesManager to the app build

--- iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
+++ iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct iOS_FakeNFT_ExtendedApp: App {
     @State private var cartStore = CartStore()
     @State private var userProfileStore = UserProfileStore()
+    @State private var likesManager: UserLikesManager
     
     @State private var startVM: AppStartViewModel
     
@@ -14,14 +15,23 @@ struct iOS_FakeNFT_ExtendedApp: App {
     
     init() {
         let userProfileStore = UserProfileStore()
+        
         let services = ServicesAssembly(
             networkClient: DefaultNetworkClient(),
             nftStorage: NftStorageImpl()
         )
+        
+        let likesManager = UserLikesManager(
+            store: userProfileStore,
+            service: services.userProfileService
+        )
+        
         _userProfileStore = State(initialValue: userProfileStore)
+        _likesManager = State(initialValue: likesManager)
         _startVM = State(initialValue: AppStartViewModel(
             services: services,
-            profileStore: userProfileStore
+            profileStore: userProfileStore,
+            likesManager: likesManager
         ))
     }
     
@@ -34,9 +44,11 @@ struct iOS_FakeNFT_ExtendedApp: App {
                 .environment(services)
                 .environment(cartStore)
                 .environment(userProfileStore)
+                .environment(likesManager)
             } else {
                 SplashScreenView()
                     .environment(startVM)
+                    .environment(likesManager)
             }
         }
     }
